ca61d82b226a733d4bdab24e263cfe54c590d352
Pratyush Mishra <pratyushmishra@berkeley.edu>
2020-11-11 20:31:51 -0800

    Fix benchmark compilation and add benchmarks for Edwards curves

---
+++ curve-benches/src/curves/ed_on_bls12_381.rs
@@ -0,0 +1,18 @@
+use rand::SeedableRng;
+use rand_xorshift::XorShiftRng;
+use std::ops::{AddAssign, MulAssign, SubAssign};
+
+use ark_ec::ProjectiveCurve;
+use ark_ed_on_bls12_381::{fq::Fq, fr::Fr, EdwardsAffine as GAffine, EdwardsProjective as G};
+use ark_ff::{
+    biginteger::{BigInteger256 as FrRepr, BigInteger256 as FqRepr},
+    BigInteger, Field, PrimeField, SquareRootField, UniformRand,
+};
+
+mod g {
+    use super::*;
+    ec_bench!(G, GAffine);
+}
+
+f_bench!(Fq, Fq, FqRepr, FqRepr, fq);
+f_bench!(Fr, Fr, FrRepr, FrRepr, fr);
--- curve-benches/src/curves/mod.rs
+++ curve-benches/src/curves/mod.rs
@@ -1,18 +1,20 @@
-#[cfg(feature = "bls12_377")]
+#[cfg(feature = "ark-bls12-377")]
 mod bls12_377;
-#[cfg(feature = "bls12_381")]
+#[cfg(feature = "ark-bls12-381")]
 mod bls12_381;
-#[cfg(feature = "bn254")]
+#[cfg(feature = "ark-bn254")]
 mod bn254;
-#[cfg(feature = "bw6_761")]
+#[cfg(feature = "ark-bw6-761")]
 mod bw6_761;
-#[cfg(feature = "cp6_782")]
+#[cfg(feature = "ark-cp6-782")]
 mod cp6_782;
-#[cfg(feature = "mnt4_298")]
+#[cfg(feature = "ark-ed-on-bls12-381")]
+mod ed_on_bls12_381;
+#[cfg(feature = "ark-mnt4-298")]
 mod mnt4_298;
-#[cfg(feature = "mnt4_753")]
+#[cfg(feature = "ark-mnt4-753")]
 mod mnt4_753;
-#[cfg(feature = "mnt6_298")]
+#[cfg(feature = "ark-mnt6-298")]
 mod mnt6_298;
-#[cfg(feature = "mnt6_753")]
+#[cfg(feature = "ark-mnt6-753")]
 mod mnt6_753;
