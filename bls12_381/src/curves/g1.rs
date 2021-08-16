ca6fe2af27b2fbe5cb5ecb1dea457fde7eae948b
Simon Masson <simon.masson@yahoo.fr>
2021-08-16 18:48:42 +0200

    subgroup check g1 with youssef EH s trick

--- bls12_381/src/curves/g1.rs
+++ bls12_381/src/curves/g1.rs
@@ -46,17 +46,11 @@ impl SWModelParameters for Parameters {
     fn is_in_correct_subgroup_assuming_on_curve_fast(
 	p : &GroupAffine<Self>
     ) -> Option<bool> {
-	// Youssef El Housni trick
 	// r = (u⁴ - u² + 1) = u² * (u²-1) + 1 = u² * lambda + 1
 	// [r]P = 0 iff [u²]psi(P) + P = 0
-	let beta =
-	    field_new!(Fq,"4002409555221667392624310435006688643935503118305586438271171395842971157480381377015405980053539358417135540939436");
-	let mut sigma_p = *p;
-	sigma_p.x *= beta;
-	let multiplier_g1 = field_new!(Fr,
-				       "228988810152649578064853576960394133504");
+	let sigma_p = psi(p);
 	let mul_sigma_p:GroupAffine<_> =
-	    sigma_p.mul(multiplier_g1).into();
+	    sigma_p.mul(MULTIPLIER_G1).into();
 	Some((mul_sigma_p+*p).is_zero())
     }
 }
@@ -70,3 +64,14 @@ pub const G1_GENERATOR_X: Fq = field_new!(Fq, "368541675371338701678108831518307
 /// 1339506544944476473020471379941921221584933875938349620426543736416511423956333506472724655353366534992391756441569
 #[rustfmt::skip]
 pub const G1_GENERATOR_Y: Fq = field_new!(Fq, "1339506544944476473020471379941921221584933875938349620426543736416511423956333506472724655353366534992391756441569");
+
+pub const MULTIPLIER_G1:Fr = field_new!(Fr, "228988810152649578064853576960394133504");
+
+pub const BETA: Fq = field_new!(Fq,"4002409555221667392624310435006688643935503118305586438271171395842971157480381377015405980053539358417135540939436");	
+
+pub fn psi(p: &GroupAffine<Parameters>)-> GroupAffine<Parameters> {
+    let mut sigma_p = *p;
+    sigma_p.x *= BETA;
+    sigma_p
+}
+    
