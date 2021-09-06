f764b7549265d07fed09bb4f69de2e222f4d4b55
Simon Masson <simon.masson@yahoo.fr>
2021-09-06 10:27:45 +0200

    confusing variable name in tests

--- bls12_381/src/curves/tests.rs
+++ bls12_381/src/curves/tests.rs
@@ -122,21 +122,21 @@ fn test_g1_generator_raw() {
 
 #[test]
 fn test_psi() {
-    let p = G2Affine::prime_subgroup_generator();
-    let mut psi_p = g2::psi(&p);
-    psi_p = g2::psi(&psi_p);
-    let psi2_p = g2::psi2(&p);
-    assert_eq!(psi_p, psi2_p);
-    psi_p = g2::psi(&psi_p);
-    let psi3_p = g2::psi3(&p);
-    assert_eq!(psi3_p, psi_p);
+    let generator = G2Affine::prime_subgroup_generator();
+    let mut psi_generator = g2::psi(&generator);
+    psi_generator = g2::psi(&psi_generator);
+    let psi2_generator = g2::psi2(&generator);
+    assert_eq!(psi_generator, psi2_generator);
+    psi_generator = g2::psi(&psi_generator);
+    let psi3_generator = g2::psi3(&generator);
+    assert_eq!(psi3_generator, psi_generator);
 }
 
 #[test]
 fn test_sigma() {
-    let p = G1Affine::prime_subgroup_generator();
-    let mut sigma_p = g1::sigma(&p);
-    sigma_p = g1::sigma(&sigma_p);
-    sigma_p = g1::sigma(&sigma_p);
-    assert_eq!(sigma_p, p);
+    let generator = G1Affine::prime_subgroup_generator();
+    let mut sigma_generator = g1::sigma(&generator);
+    sigma_generator = g1::sigma(&sigma_generator);
+    sigma_generator = g1::sigma(&sigma_generator);
+    assert_eq!(sigma_generator, generator);
 }
