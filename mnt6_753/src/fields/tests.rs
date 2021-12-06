2d0e4d349554e53c8c7ce8a5ea5da6a43d4b5775
huyuncong <huyuncongh@gmail.com>
2021-12-05 22:49:57 -0800

    Simplify the field and curve tests using macros

--- mnt6_753/src/fields/tests.rs
+++ mnt6_753/src/fields/tests.rs
@@ -2,32 +2,15 @@ use ark_ff::{
     fields::{models::fp6_2over3::*, quadratic_extension::QuadExtParameters},
     Field,
 };
-use ark_std::rand::Rng;
-use ark_std::test_rng;
+use ark_std::{rand::Rng, test_rng};
 
 use crate::*;
 
-use ark_algebra_test_templates::fields::*;
+use ark_algebra_test_templates::{fields::*, generate_field_test};
 
-#[test]
-fn test_fr() {
-    let mut rng = test_rng();
-    let a: Fr = rng.gen();
-    let b: Fr = rng.gen();
-    field_test(a, b);
-    sqrt_field_test(a);
-    primefield_test::<Fr>();
-}
+use core::ops::{AddAssign, MulAssign, SubAssign};
 
-#[test]
-fn test_fq() {
-    let mut rng = test_rng();
-    let a: Fq = rng.gen();
-    let b: Fq = rng.gen();
-    field_test(a, b);
-    sqrt_field_test(a);
-    primefield_test::<Fq>();
-}
+generate_field_test!(mnt6_753;);
 
 #[test]
 fn test_fq3() {
