6265f67ee2276b01572306a38f44f10a70768adb
Simon Masson <simon.masson@yahoo.fr>
2022-05-02 18:42:14 +0200

    λ param and remove tests in bls12_{377,381}

--- bls12_377/src/curves/glv.rs
+++ bls12_377/src/curves/glv.rs
@@ -25,6 +25,8 @@ impl GLVParameters for Parameters {
 
     const COEFF_C2: Self::BaseField = MontFp!(Fq, "0");
 
+    const LAMBDA: Self::ScalarField = MontFp!(Fr, "8444461749428370424248824938781546531284005582649182570233710176290576793600");
+
     // LLL Matrix
     const COEFF_N11: Self::ScalarField = MontFp!(Fr, "91893752504881257701523279626832445441");
 
@@ -37,83 +39,5 @@ impl GLVParameters for Parameters {
     /// Mapping a point G to phi(G):= lambda G where phi is the endomorphism
     fn endomorphism(base: &Self::CurveAffine) -> Self::CurveAffine {
         Self::CurveAffine::new(Self::COEFF_B1 * base.x, base.y, false)
-    }
-}
-
-#[test]
-fn test_scalar_decomposition() {
-    use ark_ec::ModelParameters;
-    use ark_ff::One;
-    use ark_std::UniformRand;
-    use rand::thread_rng;
-
-    let lambda = MontFp!(
-        Fr,
-        "8444461749428370424248824938781546531284005582649182570233710176290576793600"
-    );
-    assert_eq!(lambda * lambda * lambda, Fr::one());
-
-    let mut rng = thread_rng();
-    for _i in 0..100 {
-        let k = <Parameters as ModelParameters>::ScalarField::rand(&mut rng);
-        let (k1, is_k1_positive, k2, is_k2_positive) =
-            <Parameters as GLVParameters>::scalar_decomposition(&k);
-        if is_k1_positive && is_k2_positive {
-            assert_eq!(k1 + k2 * lambda, k);
-        }
-        if is_k1_positive && !is_k2_positive {
-            assert_eq!(k1 - k2 * lambda, k);
-        }
-        if !is_k1_positive && is_k2_positive {
-            assert_eq!(-k1 + k2 * lambda, k);
-        }
-        if !is_k1_positive && !is_k2_positive {
-            assert_eq!(-k1 - k2 * lambda, k);
-        }
-        // could be nice to check if k1 and k2 are indeed small.
-    }
-}
-
-#[test]
-fn test_beta() {
-    // curve of j-invariant 0 so the endomorphism is (x,y) -> (βx, y) where β is a
-    // 3rd root of unity.
-    use ark_ec::ModelParameters;
-    use ark_ff::{Field, One};
-    assert_eq!(
-        Parameters::COEFF_B1.pow(&[3u64]),
-        <Parameters as ModelParameters>::BaseField::one()
-    );
-    assert_ne!(
-        Parameters::COEFF_B1,
-        <Parameters as ModelParameters>::BaseField::one()
-    );
-}
-
-#[test]
-fn test_endomorphism() {
-    // check that `endomorphism³(P)` is zero
-    use ark_ec::{short_weierstrass_jacobian::GroupAffine, AffineCurve};
-    let g = GroupAffine::<Parameters>::prime_subgroup_generator();
-    let psi_g = <Parameters as GLVParameters>::endomorphism(&g);
-    let psi2_g = <Parameters as GLVParameters>::endomorphism(&psi_g);
-    let psi3_g = <Parameters as GLVParameters>::endomorphism(&psi2_g);
-    assert_eq!(psi3_g, g);
-}
-
-#[test]
-fn test_glv() {
-    // check that glv_mul indeed computes the scalar multiplication
-    use ark_ec::{
-        short_weierstrass_jacobian::GroupAffine, AffineCurve, ModelParameters, ProjectiveCurve,
-    };
-    use ark_std::UniformRand;
-    use rand::thread_rng;
-    let g = GroupAffine::<Parameters>::prime_subgroup_generator();
-    let mut rng = thread_rng();
-    for _i in 0..100 {
-        let k = <Parameters as ModelParameters>::ScalarField::rand(&mut rng);
-        let k_g = <Parameters as GLVParameters>::glv_mul(&g, &k).into_affine();
-        assert_eq!(k_g, g.mul(k));
     }
 }
--- bls12_381/src/curves/glv.rs
+++ bls12_381/src/curves/glv.rs
@@ -25,7 +25,8 @@ impl GLVParameters for Parameters {
 
     const COEFF_C2: Self::BaseField = MontFp!(Fq, "0");
 
-    // LLL Matrix
+    const LAMBDA: Self::ScalarField = MontFp!(Fr, "52435875175126190479447740508185965837461563690374988244538805122978187051009");
+
     const COEFF_N11: Self::ScalarField = MontFp!(Fr, "228988810152649578064853576960394133504");
 
     const COEFF_N12: Self::ScalarField = MontFp!(Fr, "1");
@@ -34,89 +35,7 @@ impl GLVParameters for Parameters {
 
     const COEFF_N22: Self::ScalarField = MontFp!(Fr, "228988810152649578064853576960394133503");
 
-    /// Mapping a point G to phi(G):= lambda G where phi is the endomorphism
     fn endomorphism(base: &Self::CurveAffine) -> Self::CurveAffine {
         Self::CurveAffine::new(Self::COEFF_B1 * base.x, base.y, false)
-    }
-}
-
-#[test]
-fn test_scalar_decomposition() {
-    use ark_ec::ModelParameters;
-    use ark_ff::One;
-    use ark_std::UniformRand;
-
-    let lambda = MontFp!(
-        Fr,
-        "52435875175126190479447740508185965837461563690374988244538805122978187051009"
-    );
-    assert_eq!(lambda * lambda * lambda, Fr::one());
-
-    let mut rng = ark_std::test_rng();
-    for _i in 0..100 {
-        let k = <Parameters as ModelParameters>::ScalarField::rand(&mut rng);
-        let (k1, is_k1_positive, k2, is_k2_positive) =
-            <Parameters as GLVParameters>::scalar_decomposition(&k);
-        if is_k1_positive && is_k2_positive {
-            assert_eq!(k1 + k2 * lambda, k);
-        }
-        if is_k1_positive && !is_k2_positive {
-            assert_eq!(k1 - k2 * lambda, k);
-        }
-        if !is_k1_positive && is_k2_positive {
-            assert_eq!(-k1 + k2 * lambda, k);
-        }
-        if !is_k1_positive && !is_k2_positive {
-            assert_eq!(-k1 - k2 * lambda, k);
-        }
-        // could be nice to check if k1 and k2 are indeed small.
-    }
-}
-
-#[test]
-fn test_beta() {
-    // curve of j-invariant 0 so the endomorphism is (x,y) -> (βx, y) where β is a
-    // 3rd root of unity.
-    use ark_ec::ModelParameters;
-    use ark_ff::{Field, One};
-    assert_eq!(
-        Parameters::COEFF_B1.pow(&[3u64]),
-        <Parameters as ModelParameters>::BaseField::one()
-    );
-    assert_ne!(
-        Parameters::COEFF_B1,
-        <Parameters as ModelParameters>::BaseField::one()
-    );
-}
-
-#[test]
-fn test_endomorphism() {
-    // check that `endomorphism³(P)` is zero
-    use ark_ec::{short_weierstrass_jacobian::GroupAffine, AffineCurve, ProjectiveCurve};
-    let g = GroupAffine::<Parameters>::prime_subgroup_generator();
-    let psi_g = <Parameters as GLVParameters>::endomorphism(&g);
-    let psi2_g = <Parameters as GLVParameters>::endomorphism(&psi_g);
-    let psi3_g = <Parameters as GLVParameters>::endomorphism(&psi2_g);
-    assert_eq!(psi3_g, g);
-    let lambda: Fr = MontFp!(
-        Fr,
-        "52435875175126190479447740508185965837461563690374988244538805122978187051009"
-    );
-    assert_eq!(psi_g, g.mul(lambda).into_affine());
-}
-
-#[test]
-fn test_glv() {
-    // check that glv_mul indeed computes the scalar multiplication
-    use ark_ec::{
-        short_weierstrass_jacobian::GroupAffine, AffineCurve, ModelParameters, ProjectiveCurve,
-    };
-    use ark_std::UniformRand;
-    let g = GroupAffine::<Parameters>::prime_subgroup_generator();
-    let mut rng = ark_std::test_rng();
-    for _i in 0..100 {
-        let k = <Parameters as ModelParameters>::ScalarField::rand(&mut rng);
-        let k_g = <Parameters as GLVParameters>::glv_mul(&g, &k).into_affine();
-        assert_eq!(k_g, g.mul(k).into_affine());
     }
 }
