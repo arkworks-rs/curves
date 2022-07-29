256b5d68537a182a51ead734a58721f4746a9a6a
Pratyush Mishra <pratyushmishra@berkeley.edu>
2022-07-29 11:46:38 -0700

    Fmt

--- ed_on_bn254/src/curves/mod.rs
+++ ed_on_bn254/src/curves/mod.rs
@@ -2,7 +2,7 @@ use ark_ec::{
     models::CurveConfig,
     twisted_edwards::{Affine, MontCurveConfig, Projective, TECurveConfig},
 };
-use ark_ff::{MontFp, Field};
+use ark_ff::{Field, MontFp};
 
 use crate::{Fq, Fr};
 
