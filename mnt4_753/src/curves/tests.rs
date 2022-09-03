dc555882cd867b1e5b6fb16f840ebb0b336136d1
Weikeng Chen <w.k@berkeley.edu>
2022-09-03 00:34:13 -0700

    Fix the remaining curve and field tests (#118)
    * push
    
    * push
    
    * cargo ready
    
    Co-authored-by: onewayfunc <onewayfunc@gmail.com>

--- mnt4_753/src/curves/tests.rs
+++ mnt4_753/src/curves/tests.rs
@@ -1,15 +1,7 @@
-use ark_algebra_test_templates::{
-    curves::*, generate_bilinearity_test, generate_g1_test, generate_g2_test,
-    generate_product_of_pairings_test, msm::*,
-};
-use ark_ec::{AffineRepr, PairingEngine, ProjectiveCurve};
-use ark_ff::{Field, One, PrimeField, UniformRand};
-use ark_std::{rand::Rng, test_rng};
-use core::ops::MulAssign;
-
 use crate::*;
+use ark_algebra_test_templates::*;
 
-generate_g1_test!(mnt4_753; curve_tests; sw_tests;);
-generate_g2_test!(mnt4_753; curve_tests; sw_tests;);
-generate_bilinearity_test!(MNT4_753, Fq4);
-generate_product_of_pairings_test!(MNT4_753);
+test_group!(g1; G1Projective; sw);
+test_group!(g2; G2Projective; sw);
+test_group!(pairing_output; ark_ec::pairing::PairingOutput<MNT4_753>; msm);
+test_pairing!(pairing; crate::MNT4_753);
