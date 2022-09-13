68f500da0161d9f48edc753bc6356829f9e9c895
Pratyush Mishra <pratyushmishra@berkeley.edu>
2022-09-12 22:03:00 -0700

    Optimizations to field and curve arithmetic

--- bls12_377/src/fields/fq2.rs
+++ bls12_377/src/fields/fq2.rs
@@ -21,10 +21,34 @@ impl Fp2Config for Fq2Config {
     ];
 
     #[inline(always)]
-    fn mul_fp_by_nonresidue(fe: &Self::Fp) -> Self::Fp {
-        let original = fe;
-        let mut fe = -fe.double();
+    fn mul_fp_by_nonresidue_in_place(fe: &mut Self::Fp) -> &mut Self::Fp {
+        let original = *fe;
+        fe.neg_in_place();
         fe.double_in_place();
-        fe - original
+        fe.double_in_place();
+        *fe -= original;
+        fe
+    }
+
+    #[inline(always)]
+    fn sub_and_mul_fp_by_nonresidue(y: &mut Self::Fp, x: &Self::Fp) {
+        let mut original = *y;
+        original += x;
+        y.double_in_place().double_in_place();
+        *y += original;
+    }
+
+    #[inline(always)]
+    fn add_and_mul_fp_by_nonresidue_plus_one(y: &mut Self::Fp, x: &Self::Fp) {
+        y.double_in_place().double_in_place().neg_in_place();
+        *y += x;
+    }
+
+    fn add_and_mul_fp_by_nonresidue_in_place(y: &mut Self::Fp, x: &Self::Fp) {
+        let mut original = *y;
+        original.double_in_place().double_in_place();
+        original += &*y;
+        *y = *x;
+        *y -= original;
     }
 }
