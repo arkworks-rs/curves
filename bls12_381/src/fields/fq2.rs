1833cbfb298526f880fb3df0d6fe2952586258da
Pratyush Mishra <pratyushmishra@berkeley.edu>
2022-09-22 14:11:14 -0700

    Tweaks

--- bls12_381/src/fields/fq2.rs
+++ bls12_381/src/fields/fq2.rs
@@ -35,7 +35,7 @@ impl Fp2Config for Fq2Config {
         *y = *x;
     }
 
-    fn mul_fp_by_nonresidue_in_place_and_add(y: &mut Self::Fp, x: &Self::Fp) {
+    fn mul_fp_by_nonresidue_and_add(y: &mut Self::Fp, x: &Self::Fp) {
         y.neg_in_place();
         *y += x;
     }
