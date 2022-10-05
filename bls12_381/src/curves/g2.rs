5c0780dcb5c95ae0197945f5a4dc82ecabf2e99e
Skalman <skalman@riseup.net>
2022-10-05 10:08:20 -0400

    adapt bls12 wb hashing to new changes in algebra

--- bls12_381/src/curves/g2.rs
+++ bls12_381/src/curves/g2.rs
@@ -1,19 +1,19 @@
-use ark_ec::{
-    bls12,
-    bls12::Bls12Parameters,
-    models::CurveConfig,
-    short_weierstrass::{Affine, SWCurveConfig},
-    AffineRepr,
-};
-use ark_ff::{Field, MontFp, Zero};
+use core::ops::Neg;
 
 use crate::*;
+use super::g2_swu_iso;
 
-use ark_ec::hashing::curve_maps::wb::WBParams;
-use super::g2_swu_iso::Parameters as SWUIsogenousCurveParameters;
+use ark_ec::{
+    bls12::{self, Bls12Parameters},
+    hashing::curve_maps::wb::WBParams,
+    models::CurveConfig,
+    short_weierstrass::{self, *},
+    AffineRepr, CurveGroup, Group,
+};
+use ark_ff::{BigInt, Field, MontFp, Zero};
 
-pub type G2Affine = bls12::G2Affine<crate::Parameters>;
-pub type G2Projective = bls12::G2Projective<crate::Parameters>;
+pub type G2Affine = bls12::G2Affine<super::Parameters>;
+pub type G2Projective = bls12::G2Projective<super::Parameters>;
 
 #[derive(Clone, Default, PartialEq, Eq)]
 pub struct Parameters;
@@ -39,11 +39,13 @@ impl CurveConfig for Parameters {
 
     /// COFACTOR_INV = COFACTOR^{-1} mod r
     /// 26652489039290660355457965112010883481355318854675681319708643586776743290055
-    const COFACTOR_INV: Fr =
-        MontFp!("26652489039290660355457965112010883481355318854675681319708643586776743290055");
+    #[rustfmt::skip]
+    const COFACTOR_INV: Fr = MontFp!(
+        "26652489039290660355457965112010883481355318854675681319708643586776743290055"
+    );
 }
 
-impl SWCurveConfig for Parameters {
+impl short_weierstrass::SWCurveConfig for Parameters {
     /// COEFF_A = [0, 0]
     const COEFF_A: Fq2 = Fq2::new(g1::Parameters::COEFF_A, g1::Parameters::COEFF_A);
 
@@ -63,14 +65,44 @@ impl SWCurveConfig for Parameters {
         //
         // Checks that [p]P = [X]P
 
-        let mut x_times_point = point.mul_bigint(crate::Parameters::X);
-        if crate::Parameters::X_IS_NEGATIVE {
+        let mut x_times_point =
+            point.mul_bigint(BigInt::new([super::Parameters::X[0], 0, 0, 0]));
+        if super::Parameters::X_IS_NEGATIVE {
             x_times_point = -x_times_point;
         }
 
         let p_times_point = p_power_endomorphism(point);
 
         x_times_point.eq(&p_times_point)
+    }
+
+    #[inline]
+    fn clear_cofactor(p: &G2Affine) -> G2Affine {
+        // Based on Section 4.1 of https://eprint.iacr.org/2017/419.pdf
+        // [h(ψ)]P = [x^2 − x − 1]P + [x − 1]ψ(P) + (ψ^2)(2P)
+
+        // x = -15132376222941642752
+        // When multiplying, use -c1 instead, and then negate the result. That's much
+        // more efficient, since the scalar -c1 has less limbs and a much lower Hamming
+        // weight.
+        let x: &'static [u64] = super::Parameters::X;
+        let p_projective = p.into_group();
+
+        // [x]P
+        let x_p = Parameters::mul_affine(p, x).neg();
+        // ψ(P)
+        let psi_p = p_power_endomorphism(p);
+        // (ψ^2)(2P)
+        let mut psi2_p2 = double_p_power_endomorphism(&p_projective.double());
+
+        // tmp = [x^2]P + [x]ψ(P)
+        let tmp = (x_p + psi_p).mul_bigint(x).neg();
+
+        // add up all the terms
+        psi2_p2 += tmp;
+        psi2_p2 -= x_p;
+        psi2_p2 -= psi_p;
+        (psi2_p2 - p_projective).into_affine()
     }
 }
 
@@ -79,18 +111,22 @@ pub const G2_GENERATOR_Y: Fq2 = Fq2::new(G2_GENERATOR_Y_C0, G2_GENERATOR_Y_C1);
 
 /// G2_GENERATOR_X_C0 =
 /// 352701069587466618187139116011060144890029952792775240219908644239793785735715026873347600343865175952761926303160
+#[rustfmt::skip]
 pub const G2_GENERATOR_X_C0: Fq = MontFp!("352701069587466618187139116011060144890029952792775240219908644239793785735715026873347600343865175952761926303160");
 
 /// G2_GENERATOR_X_C1 =
 /// 3059144344244213709971259814753781636986470325476647558659373206291635324768958432433509563104347017837885763365758
+#[rustfmt::skip]
 pub const G2_GENERATOR_X_C1: Fq = MontFp!("3059144344244213709971259814753781636986470325476647558659373206291635324768958432433509563104347017837885763365758");
 
 /// G2_GENERATOR_Y_C0 =
 /// 1985150602287291935568054521177171638300868978215655730859378665066344726373823718423869104263333984641494340347905
+#[rustfmt::skip]
 pub const G2_GENERATOR_Y_C0: Fq = MontFp!("1985150602287291935568054521177171638300868978215655730859378665066344726373823718423869104263333984641494340347905");
 
 /// G2_GENERATOR_Y_C1 =
 /// 927553665492332455747201965776037880757740193453592970025027978793976877002675564980949289727957565575433344219582
+#[rustfmt::skip]
 pub const G2_GENERATOR_Y_C1: Fq = MontFp!("927553665492332455747201965776037880757740193453592970025027978793976877002675564980949289727957565575433344219582");
 
 // psi(x,y) = (x**p * PSI_X, y**p * PSI_Y) is the Frobenius composed
@@ -98,29 +134,32 @@ pub const G2_GENERATOR_Y_C1: Fq = MontFp!("9275536654923324557472019657760378807
 
 // PSI_X = 1/(u+1)^((p-1)/3)
 pub const P_POWER_ENDOMORPHISM_COEFF_0 : Fq2 = Fq2::new(
-    Fq::ZERO,
+    FQ_ZERO,
     MontFp!(
-                "4002409555221667392624310435006688643935503118305586438271171395842971157480381377015405980053539358417135540939437"
+       "4002409555221667392624310435006688643935503118305586438271171395842971157480381377015405980053539358417135540939437"
     )
 );
 
 // PSI_Y = 1/(u+1)^((p-1)/2)
 pub const P_POWER_ENDOMORPHISM_COEFF_1: Fq2 = Fq2::new(
     MontFp!(
-                "2973677408986561043442465346520108879172042883009249989176415018091420807192182638567116318576472649347015917690530"),
+       "2973677408986561043442465346520108879172042883009249989176415018091420807192182638567116318576472649347015917690530"),
     MontFp!(
-                "1028732146235106349975324479215795277384839936929757896155643118032610843298655225875571310552543014690878354869257")
+       "1028732146235106349975324479215795277384839936929757896155643118032610843298655225875571310552543014690878354869257")
+);
+
+pub const DOUBLE_P_POWER_ENDOMORPHISM: Fq2 = Fq2::new(
+    MontFp!("4002409555221667392624310435006688643935503118305586438271171395842971157480381377015405980053539358417135540939436"),
+    FQ_ZERO
 );
 
 pub fn p_power_endomorphism(p: &Affine<Parameters>) -> Affine<Parameters> {
     // The p-power endomorphism for G2 is defined as follows:
-    // 1. Note that G2 is defined on curve E': y^2 = x^3 + 4(u+1).
-    //    To map a point (x, y) in E' to (s, t) in E,
-    //    one set s = x / ((u+1) ^ (1/3)), t = y / ((u+1) ^ (1/2)),
-    //    because E: y^2 = x^3 + 4.
-    // 2. Apply the Frobenius endomorphism (s, t) => (s', t'),
-    //    another point on curve E, where s' = s^p, t' = t^p.
-    // 3. Map the point from E back to E'; that is,
+    // 1. Note that G2 is defined on curve E': y^2 = x^3 + 4(u+1). To map a point
+    // (x, y) in E' to (s, t) in E,    one set s = x / ((u+1) ^ (1/3)), t = y /
+    // ((u+1) ^ (1/2)), because E: y^2 = x^3 + 4. 2. Apply the Frobenius
+    // endomorphism (s, t) => (s', t'), another point on curve E,    where s' =
+    // s^p, t' = t^p. 3. Map the point from E back to E'; that is,
     //    one set x' = s' * ((u+1) ^ (1/3)), y' = t' * ((u+1) ^ (1/2)).
     //
     // To sum up, it maps
@@ -131,72 +170,82 @@ pub fn p_power_endomorphism(p: &Affine<Parameters>) -> Affine<Parameters> {
     res.x.frobenius_map(1);
     res.y.frobenius_map(1);
 
-    let tmp_x = res.x.clone();
-    res.x.c0 = -P_POWER_ENDOMORPHISM_COEFF_0.c1 * &tmp_x.c1;
-    res.x.c1 = P_POWER_ENDOMORPHISM_COEFF_0.c1 * &tmp_x.c0;
+    let tmp_x = res.x;
+    res.x.c0 = -P_POWER_ENDOMORPHISM_COEFF_0.c1 * tmp_x.c1;
+    res.x.c1 = P_POWER_ENDOMORPHISM_COEFF_0.c1 * tmp_x.c0;
     res.y *= P_POWER_ENDOMORPHISM_COEFF_1;
 
     res
 }
 
-impl WBParams for Parameters
-{
-    type IsogenousCurve = SWUIsogenousCurveParameters;
+/// For a p-power endomorphism psi(P), compute psi(psi(P))
+pub fn double_p_power_endomorphism(p: &Projective<Parameters>) -> Projective<Parameters> {
+    let mut res = *p;
 
-    const PHI_X_NOM: &'static [<Self::IsogenousCurve as ModelParameters>::BaseField] = &[
-        QuadExt!(
-                   MontFp!(Fq, "889424345604814976315064405719089812568196182208668418962679585805340366775741747653930584250892369786198727235542"), 
-                   MontFp!(Fq, "889424345604814976315064405719089812568196182208668418962679585805340366775741747653930584250892369786198727235542")), 
-        QuadExt!(
-                   MontFp!(Fq, "0"), 
-                   MontFp!(Fq, "2668273036814444928945193217157269437704588546626005256888038757416021100327225242961791752752677109358596181706522")), 
-        QuadExt!(
-                   MontFp!(Fq, "2668273036814444928945193217157269437704588546626005256888038757416021100327225242961791752752677109358596181706526"), 
-                   MontFp!(Fq, "1334136518407222464472596608578634718852294273313002628444019378708010550163612621480895876376338554679298090853261")), 
-        QuadExt!(
-                   MontFp!(Fq, "3557697382419259905260257622876359250272784728834673675850718343221361467102966990615722337003569479144794908942033"), 
-                   MontFp!(Fq, "0")),
+    res.x *= DOUBLE_P_POWER_ENDOMORPHISM;
+    res.y = res.y.neg();
+
+    res
+}
+
+// Parameters from the [IETF draft v16, section E.3](https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-16.html#name-3-isogeny-map-for-bls12-381).
+impl WBParams for Parameters {
+    type IsogenousCurve = g2_swu_iso::SwuIsoParameters;
+
+    const PHI_X_NOM: &'static [<Self::IsogenousCurve as CurveConfig>::BaseField] = &[
+        Fq2::new(
+                   MontFp!("889424345604814976315064405719089812568196182208668418962679585805340366775741747653930584250892369786198727235542"), 
+                   MontFp!("889424345604814976315064405719089812568196182208668418962679585805340366775741747653930584250892369786198727235542")), 
+        Fq2::new(
+                   MontFp!("0"), 
+                   MontFp!("2668273036814444928945193217157269437704588546626005256888038757416021100327225242961791752752677109358596181706522")), 
+        Fq2::new(
+                   MontFp!("2668273036814444928945193217157269437704588546626005256888038757416021100327225242961791752752677109358596181706526"), 
+                   MontFp!("1334136518407222464472596608578634718852294273313002628444019378708010550163612621480895876376338554679298090853261")), 
+        Fq2::new(
+                   MontFp!("3557697382419259905260257622876359250272784728834673675850718343221361467102966990615722337003569479144794908942033"), 
+                   MontFp!("0")),
     ];
-    
-    const PHI_X_DEN: &'static [<Self::IsogenousCurve as ModelParameters>::BaseField] = &[
-        QuadExt!(
-                   MontFp!(Fq, "0"), 
-                   MontFp!(Fq, "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559715")), 
-        QuadExt!(
-                   MontFp!(Fq, "12"), 
-                   MontFp!(Fq, "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559775")), 
-        QuadExt!(
-                   MontFp!(Fq, "1"), 
-                   MontFp!(Fq, "0")),
+
+    const PHI_X_DEN: &'static [<Self::IsogenousCurve as CurveConfig>::BaseField] = &[
+        Fq2::new(
+                   MontFp!("0"), 
+                   MontFp!("4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559715")), 
+        Fq2::new(
+                   MontFp!("12"), 
+                   MontFp!("4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559775")), 
+        Fq2::new(
+                   MontFp!("1"), 
+                   MontFp!("0")),
     ];
-    
-    const PHI_Y_NOM: &'static [<Self::IsogenousCurve as ModelParameters>::BaseField] = &[
-        QuadExt!(
-                   MontFp!(Fq, "741186954670679146929220338099241510473496818507223682468899654837783638979784789711608820209076974821832272696229"), 
-                   MontFp!(Fq, "741186954670679146929220338099241510473496818507223682468899654837783638979784789711608820209076974821832272696229")), 
-        QuadExt!(
-                   MontFp!(Fq, "0"), 
-                   MontFp!(Fq, "3112985209616852417102725420016814343988686637730339466369378550318691283715096116788757044878123294251695545324269")), 
-        QuadExt!(
-                   MontFp!(Fq, "1334136518407222464472596608578634718852294273313002628444019378708010550163612621480895876376338554679298090853263"), 
-                   MontFp!(Fq, "2668273036814444928945193217157269437704588546626005256888038757416021100327225242961791752752677109358596181706524")), 
-        QuadExt!(
-                   MontFp!(Fq, "1185899127473086635086752540958786416757594909611557891950239447740453822367655663538574112334523159714931636314011"), 
-                   MontFp!(Fq, "0")),
+
+    const PHI_Y_NOM: &'static [<Self::IsogenousCurve as CurveConfig>::BaseField] = &[
+        Fq2::new(
+                   MontFp!("3261222600550988246488569487636662646083386001431784202863158481286248011511053074731078808919938689216061999863558"), 
+                   MontFp!("3261222600550988246488569487636662646083386001431784202863158481286248011511053074731078808919938689216061999863558")), 
+        Fq2::new(
+                   MontFp!("0"), 
+                   MontFp!("889424345604814976315064405719089812568196182208668418962679585805340366775741747653930584250892369786198727235518")), 
+        Fq2::new(
+                   MontFp!("2668273036814444928945193217157269437704588546626005256888038757416021100327225242961791752752677109358596181706524"), 
+                   MontFp!("1334136518407222464472596608578634718852294273313002628444019378708010550163612621480895876376338554679298090853263")), 
+        Fq2::new(
+                   MontFp!("2816510427748580758331037284777117739799287910327449993381818688383577828123182200904113516794492504322962636245776"), 
+                   MontFp!("0")),
     ];
-    
-    const PHI_Y_DEN: &'static [<Self::IsogenousCurve as ModelParameters>::BaseField] = &[
-        QuadExt!(
-                   MontFp!(Fq, "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559355"), 
-                   MontFp!(Fq, "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559355")), 
-        QuadExt!(
-                   MontFp!(Fq, "0"), 
-                   MontFp!(Fq, "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559571")), 
-        QuadExt!(
-                   MontFp!(Fq, "18"), 
-                   MontFp!(Fq, "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559769")), 
-        QuadExt!(
-                   MontFp!(Fq, "1"), 
-                   MontFp!(Fq, "0")),
+
+    const PHI_Y_DEN: &'static [<Self::IsogenousCurve as CurveConfig>::BaseField] = &[
+        Fq2::new(
+                   MontFp!("4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559355"), 
+                   MontFp!("4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559355")), 
+        Fq2::new(
+                   MontFp!("0"), 
+                   MontFp!("4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559571")), 
+        Fq2::new(
+                   MontFp!("18"), 
+                   MontFp!("4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559769")), 
+        Fq2::new(
+                   MontFp!("1"), 
+                   MontFp!("0")),
     ];
 }
--- bls12_381/src/curves/g2_swu_iso.rs
+++ bls12_381/src/curves/g2_swu_iso.rs
@@ -1,18 +1,23 @@
 use crate::*;
-use ark_ec::models::{ModelParameters, SWModelParameters};
-use ark_ff::{MontFp, QuadExt};
 
-use ark_ec::hashing::curve_maps::swu::{SWUParams};
+use ark_ec::models::{
+    short_weierstrass::{Affine, SWCurveConfig},
+    CurveConfig,
+};
+use ark_ff::MontFp;
+
+use ark_ec::hashing::curve_maps::swu::SWUParams;
+
+type G2Affine = Affine<SwuIsoParameters>;
 
 #[derive(Clone, Default, PartialEq, Eq)]
-pub struct Parameters;
+pub struct SwuIsoParameters;
 
-impl ModelParameters for Parameters {
+impl CurveConfig for SwuIsoParameters {
     type BaseField = Fq2;
     type ScalarField = Fr;
 
-    //sage: g2_iso.codomain().order() == g2_iso.domain().order()
-    //True
+    /// Cofactors of g2_iso and g2 are the same.
     /// COFACTOR = (x^8 - 4 x^7 + 5 x^6) - (4 x^4 + 6 x^3 - 4 x^2 - 4 x + 13) //
     /// 9
     /// = 305502333931268344200999753193121504214466019254188142667664032982267604182971884026507427359259977847832272839041616661285803823378372096355777062779109
@@ -31,8 +36,7 @@ impl ModelParameters for Parameters {
     /// COFACTOR_INV = COFACTOR^{-1} mod r
     /// 26652489039290660355457965112010883481355318854675681319708643586776743290055
     #[rustfmt::skip]
-    const COFACTOR_INV: Fr = MontFp!(Fr, "26652489039290660355457965112010883481355318854675681319708643586776743290055");
-
+    const COFACTOR_INV: Fr = MontFp!("26652489039290660355457965112010883481355318854675681319708643586776743290055");
 }
 
 // https://datatracker.ietf.org/doc/draft-irtf-cfrg-hash-to-curve/
@@ -45,59 +49,42 @@ impl ModelParameters for Parameters {
 //      -  B' = 1012 * (1 + I)
 //
 //   * Z: -(2 + I)
+impl SWCurveConfig for SwuIsoParameters {
+    /// COEFF_A = 240 * I
+    const COEFF_A: Fq2 = Fq2::new(MontFp!("0"), MontFp!("240"));
 
-impl SWModelParameters for Parameters {
-    /// COEFF_A = [240, 0]
-    const COEFF_A: Fq2 = QuadExt!(MontFp!(Fq, "0"), MontFp!(Fq, "240"),);
+    /// COEFF_B = 1012 + 1012 * I
+    const COEFF_B: Fq2 = Fq2::new(MontFp!("1012"), MontFp!("1012"));
 
-    /// COEFF_B = [1012, 1012]
-    const COEFF_B: Fq2 = QuadExt!(MontFp!(Fq, "1012"), MontFp!(Fq, "1012"),);
-
-    /// AFFINE_GENERATOR_COEFFS = (G2_GENERATOR_X, G2_GENERATOR_Y)
-    const AFFINE_GENERATOR_COEFFS: (Self::BaseField, Self::BaseField) =
-        (G2_GENERATOR_X, G2_GENERATOR_Y);
-
+    const GENERATOR: G2Affine = G2Affine::new_unchecked(G2_GENERATOR_X, G2_GENERATOR_Y);
 }
 
-pub const G2_GENERATOR_X: Fq2 = QuadExt!(G2_GENERATOR_X_C0, G2_GENERATOR_X_C1);
-pub const G2_GENERATOR_Y: Fq2 = QuadExt!(G2_GENERATOR_Y_C0, G2_GENERATOR_Y_C1);
+/// Lexicographically smallest, valid x-coordinate of a point P on the curve (with its corresponding y) multiplied by the cofactor.
+/// P_x = 1
+/// P_y = 1199519624119946820355795551601605892701128025883245860600494152840508171012839086684258857614063467038089173303263 + 2721622435888802346851223931977585460571674503470326381323808470905804676865417627238564067834747838523978879375704 * I
+/// P = E(P_x, P_y)
+/// G = P * COFACTOR
+const G2_GENERATOR_X: Fq2 = Fq2::new(G2_GENERATOR_X_C0, G2_GENERATOR_X_C1);
+const G2_GENERATOR_Y: Fq2 = Fq2::new(G2_GENERATOR_Y_C0, G2_GENERATOR_Y_C1);
 
-// sage: gen_p = E2p.random_point()
-// sage: gen_p *= 305502333931268344200999753193121504214466019254188142667664032982267604182971884026507427359259977847832272839041616661285803823378372096355777062779109
-// sage: gen_p
-// (3742867872338536021971219193407057626975566811775929228638115610845414823755734136602223916420662890679205212867815*X + 2008970337784971958599114573863246208442825790877271736434783886837659225024554344101831904846457859657000371883048 : 347020557560111175564150279686487416538085532798697633906066128669670325197422119639641125873165591545442034801173*X + 3002750085289950562156391971969303286031832683890522668239999505933394032768942051901168474820339778663917384775384 : 1)
+const G2_GENERATOR_X_C0: Fq = MontFp!("2595569946714414516067015540153643524656442638788025933727967960306287756885400469291119095920626560658971252184199");
+const G2_GENERATOR_X_C1: Fq = MontFp!("1037079738597573406765355774006601850633656296583542639082316151670128374872040593053087014315526494961765370307992");
+const G2_GENERATOR_Y_C0: Fq = MontFp!("3927929472994661655038722055497331445175131868678630546921475383290711810401295661250673209427965906654429357114487");
+const G2_GENERATOR_Y_C1: Fq = MontFp!("3300326318345570015758639333209189167876318321385223785506096497597561910823001330832964776707374262378602791224889");
 
-#[rustfmt::skip]
-pub const G2_GENERATOR_X_C0: Fq = MontFp!(Fq, "2008970337784971958599114573863246208442825790877271736434783886837659225024554344101831904846457859657000371883048");
+impl SWUParams for SwuIsoParameters {
+    // ZETA = -(2 + u) as per IETF draft.
+    const ZETA: Fq2 = Fq2::new(MontFp!("-2"), MontFp!("-1"));
+}
+#[cfg(test)]
+mod test {
+	use super::*;
 
-/// G2_GENERATOR_X_C1 =
-#[rustfmt::skip]
-pub const G2_GENERATOR_X_C1: Fq = MontFp!(Fq, "3742867872338536021971219193407057626975566811775929228638115610845414823755734136602223916420662890679205212867815");
 
-/// G2_GENERATOR_Y_C0 =
-#[rustfmt::skip]
-pub const G2_GENERATOR_Y_C0: Fq = MontFp!(Fq, "3002750085289950562156391971969303286031832683890522668239999505933394032768942051901168474820339778663917384775384");
-
-/// G2_GENERATOR_Y_C1 =
-#[rustfmt::skip]
-pub const G2_GENERATOR_Y_C1: Fq = MontFp!(Fq, "347020557560111175564150279686487416538085532798697633906066128669670325197422119639641125873165591545442034801173");
-
-// sage: Fq2 = g2_iso.domain().base_field()
-// sage: Fq2
-// Finite Field in X of size 4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787^2
-// sage: Fq2.primitive_element()
-// X + 2
-// sage: Zeta = _
-// sage: Zeta
-// X + 2
-// sage: Xi = Fq2(-Fq2.gens()[0] - 2)
-// sage: Xi == -Zeta
-// True
-// sage: (Xi/Zeta).square_root()
-//X
-impl SWUParams for Parameters {    
-    const XI : Fq2 = QuadExt!(MontFp!(Fq, "-2"), MontFp!(Fq, "-1"),); //a nonsquare in Fq ietf standard
-    const ZETA: Fq2 = QuadExt!(MontFp!(Fq, "2"), MontFp!(Fq, "1"),);  //arbitatry primitive root of unity (element)
-    const XI_ON_ZETA_SQRT: Fq2 = QuadExt!(MontFp!(Fq, "0"), MontFp!(Fq, "1"),); ////square root of THETA=Xi/Zeta
-
+    #[test]
+    fn test_gen() {
+        let gen: G2Affine = curves::g2_swu_iso::SwuIsoParameters::GENERATOR;
+        assert!(gen.is_on_curve());
+        assert!(gen.is_in_correct_subgroup_assuming_on_curve());
+    }
 }
