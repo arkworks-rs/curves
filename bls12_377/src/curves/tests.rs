f7176b264985810c4c1f8eaecb9fbf311fe9a70d
Skalman <skalman@riseup.net>
2022-10-06 12:11:52 -0400

    add unit tests for wb hashing to bls12-377 g1 and g2

--- bls12_377/src/curves/tests.rs
+++ bls12_377/src/curves/tests.rs
@@ -5,3 +5,51 @@ test_group!(g1; G1Projective; sw);
 test_group!(g2; G2Projective; sw);
 test_group!(pairing_output; ark_ec::pairing::PairingOutput<Bls12_377>; msm);
 test_pairing!(pairing; crate::Bls12_377);
+
+#[cfg(test)]
+mod test {
+    use ark_ec::{
+        hashing::{
+            curve_maps::wb::{WBMap, WBParams},
+            map_to_curve_hasher::MapToCurveBasedHasher,
+            HashToCurve,
+        },
+        short_weierstrass::Projective,
+    };
+    use ark_ff::field_hashers::DefaultFieldHasher;
+    use ark_std::Zero;
+
+    /// make a simple hash
+    fn wb_hash_arbitrary_string_to_curve<WBCurve: WBParams>() {
+        use sha2::Sha256;
+        let test_wb_to_curve_hasher = MapToCurveBasedHasher::<
+            Projective<WBCurve>,
+            DefaultFieldHasher<Sha256, 128>,
+            WBMap<WBCurve>,
+        >::new(&[1])
+        .unwrap();
+
+        let hash_result = test_wb_to_curve_hasher.hash(b"if you stick a Babel fish in your ear you can instantly understand anything said to you in any form of language.").expect("fail to hash the string to curve");
+
+        assert!(
+            hash_result.x != WBCurve::BaseField::zero()
+                && hash_result.y != WBCurve::BaseField::zero(),
+            "we assume that not both a and b coefficienst are zero for the test curve"
+        );
+
+        assert!(
+            hash_result.is_on_curve(),
+            "hash results into a point off the curve"
+        );
+    }
+
+    #[test]
+    fn wb_hash_arbitrary_string_to_g1() {
+        wb_hash_arbitrary_string_to_curve::<crate::g1::Parameters>();
+    }
+
+    #[test]
+    fn wb_hash_arbitrary_string_to_g2() {
+        wb_hash_arbitrary_string_to_curve::<crate::g2::Parameters>();
+    }
+}
