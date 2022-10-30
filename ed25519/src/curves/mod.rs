ca9b0e93e59428f72f227c2f52e0499902a9129a
Weikeng Chen <w.k@berkeley.edu>
2022-10-29 18:37:31 -0700

    Update ed25519/src/curves/mod.rs

--- ed25519/src/curves/mod.rs
+++ ed25519/src/curves/mod.rs
@@ -35,7 +35,7 @@ impl TECurveConfig for EdwardsParameters {
     const COEFF_D: Fq =
         MontFp!("37095705934669439343138083508754565189542113879843219016388785533085940283555");
 
-    /// Standard generators from https://neuromancer.sk/std/other/Ed25519.
+    /// Standard generators from <https://neuromancer.sk/std/other/Ed25519>.
     const GENERATOR: EdwardsAffine = EdwardsAffine::new_unchecked(GENERATOR_X, GENERATOR_Y);
 
     type MontCurveConfig = EdwardsParameters;
