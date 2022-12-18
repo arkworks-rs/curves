019f539bfa2cd0c1553bee2c3fcb60689423c634
onewayfunc <onewayfunc@gmail.com>
2022-12-18 14:28:02 -0800

    fix

--- secp256k1/src/fields/tests.rs
+++ secp256k1/src/fields/tests.rs
@@ -1,5 +1,8 @@
+use std::str::FromStr;
 use crate::{Fq, Fr};
 use ark_algebra_test_templates::*;
+use ark_algebra_test_templates::num_bigint::BigUint;
+use ark_ff::{MontFp, PrimeField};
 
 test_field!(fr; Fr; mont_prime_field);
 test_field!(fq; Fq; mont_prime_field);
