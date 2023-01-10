e221e4d99e32f11c5e4840db84a1c987140402ac
Simon Masson <simon.masson@yahoo.fr>
2023-01-10 10:40:31 +0000

    pallas glv

--- pallas/src/curves/mod.rs
+++ pallas/src/curves/mod.rs
@@ -1,5 +1,6 @@
 use ark_ec::{
     models::CurveConfig,
+    scalar_mul::glv::GLVConfig,
     short_weierstrass::{self as sw, SWCurveConfig},
 };
 use ark_ff::{Field, MontFp, Zero};
@@ -42,8 +43,65 @@ impl SWCurveConfig for PallasConfig {
     }
 }
 
+impl GLVConfig for PallasConfig {
+    const COEFFS_ENDOMORPHISM: &'static [Self::BaseField] = &[MontFp!(
+        "20444556541222657078399132219657928148671392403212669005631716460534733845831"
+    )];
+
+    const LAMBDA: Self::ScalarField =
+        MontFp!("26005156700822196841419187675678338661165322343552424574062261873906994770353");
+
+    const COEFF_N: [<Self as CurveConfig>::ScalarField; 4] = [
+        MontFp!("98231058071100081932162823354453065728"),
+        MontFp!("98231058071186745657228807397848383489"),
+        MontFp!("196462116142286827589391630752301449217"),
+        MontFp!("98231058071100081932162823354453065728"),
+    ];
+    const SGN_N: [bool; 4] = [false, true, false, false];
+
+    fn endomorphism(p: &Projective) -> Projective {
+        // Endomorphism of the points on the curve.
+        // endomorphism_p(x,y) = (BETA * x, y)
+        // where BETA is a non-trivial cubic root of unity in Fq.
+        let mut res = (*p).clone();
+        res.x *= Self::COEFFS_ENDOMORPHISM[0];
+        res
+    }
+}
+
 /// G_GENERATOR_X = -1
 pub const G_GENERATOR_X: Fq = MontFp!("-1");
 
 /// G_GENERATOR_Y = 2
 pub const G_GENERATOR_Y: Fq = MontFp!("2");
+
+#[cfg(test)]
+mod test {
+
+    use std::time::Instant;
+
+    use super::*;
+    use ark_std::{test_rng, UniformRand};
+
+    #[test]
+    fn test_bench_glv() {
+        let mut rng = test_rng();
+        let p = Projective::rand(&mut rng);
+        let s = Fr::rand(&mut rng);
+        // test
+        let q = p * s;
+        let r = PallasConfig::glv_mul(p, s);
+        assert_eq!(q, r);
+        // bench
+        let now = Instant::now();
+        for _ in 1..100 {
+            let _ = p * s;
+        }
+        println!("SM: {:?}", now.elapsed());
+        let now = Instant::now();
+        for _ in 1..100 {
+            let _ = PallasConfig::glv_mul(p, s);
+        }
+        println!("GLV: {:?}", now.elapsed());
+    }
+}
