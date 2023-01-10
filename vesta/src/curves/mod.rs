6a5468d251e0795f848ee40e6a870b2b42eda701
Simon Masson <simon.masson@yahoo.fr>
2023-01-10 10:43:33 +0000

    vesta glv

--- vesta/src/curves/mod.rs
+++ vesta/src/curves/mod.rs
@@ -1,6 +1,7 @@
 use crate::{fq::Fq, fr::Fr};
 use ark_ec::{
     models::CurveConfig,
+    scalar_mul::glv::GLVConfig,
     short_weierstrass::{self as sw, SWCurveConfig},
 };
 use ark_ff::{Field, MontFp, Zero};
@@ -41,6 +42,32 @@ impl SWCurveConfig for VestaConfig {
     }
 }
 
+impl GLVConfig for VestaConfig {
+    const COEFFS_ENDOMORPHISM: &'static [Self::BaseField] = &[MontFp!(
+        "26005156700822196841419187675678338661165322343552424574062261873906994770353"
+    )];
+
+    const LAMBDA: Self::ScalarField =
+        MontFp!("20444556541222657078399132219657928148671392403212669005631716460534733845831");
+
+    const COEFF_N: [<Self as CurveConfig>::ScalarField; 4] = [
+        MontFp!("98231058071100081932162823354453065729"),
+        MontFp!("98231058071186745657228807397848383488"),
+        MontFp!("196462116142286827589391630752301449217"),
+        MontFp!("98231058071100081932162823354453065729"),
+    ];
+    const SGN_N: [bool; 4] = [false, true, false, false];
+
+    fn endomorphism(p: &Projective) -> Projective {
+        // Endomorphism of the points on the curve.
+        // endomorphism_p(x,y) = (BETA * x, y)
+        // where BETA is a non-trivial cubic root of unity in Fq.
+        let mut res = (*p).clone();
+        res.x *= Self::COEFFS_ENDOMORPHISM[0];
+        res
+    }
+}
+
 /// G_GENERATOR_X = -1
 /// Encoded in Montgomery form, so the value here is -R mod p.
 pub const G_GENERATOR_X: Fq = MontFp!("-1");
@@ -48,3 +75,34 @@ pub const G_GENERATOR_X: Fq = MontFp!("-1");
 /// G_GENERATOR_Y = 2
 /// Encoded in Montgomery form, so the value here is 2R mod p.
 pub const G_GENERATOR_Y: Fq = MontFp!("2");
+
+#[cfg(test)]
+mod test {
+
+    use std::time::Instant;
+
+    use super::*;
+    use ark_std::{test_rng, UniformRand};
+
+    #[test]
+    fn test_bench_glv() {
+        let mut rng = test_rng();
+        let p = Projective::rand(&mut rng);
+        let s = Fr::rand(&mut rng);
+        // test
+        let q = p * s;
+        let r = VestaConfig::glv_mul(p, s);
+        assert_eq!(q, r);
+        // bench
+        let now = Instant::now();
+        for _ in 1..100 {
+            let _ = p * s;
+        }
+        println!("SM: {:?}", now.elapsed());
+        let now = Instant::now();
+        for _ in 1..100 {
+            let _ = VestaConfig::glv_mul(p, s);
+        }
+        println!("GLV: {:?}", now.elapsed());
+    }
+}
