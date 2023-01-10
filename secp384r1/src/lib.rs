ac21ecfd89cc0888651e0e8154c3a9f19828fecb
Ruben De Smet <ruben.de.smet@rubdos.be>
2023-01-10 11:45:40 +0100

    Fill in remaining curve parameters in module documentation

--- secp384r1/src/lib.rs
+++ secp384r1/src/lib.rs
@@ -12,10 +12,12 @@
 //! Source: <https://neuromancer.sk/std/nist/P-384>
 //!
 //! Curve information:
-//! * Base field: q = TODO
-//! * Scalar field: r = TODO
-//! * a = TODO
-//! * b = TODO
+//! * Base field: q =
+//!   0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000ffffffff
+//! * Scalar field: r =
+//!   0xffffffffffffffffffffffffffffffffffffffffffffffffc7634d81f4372ddf581a0db248b0a77aecec196accc52973
+//! * a = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000fffffffc
+//! * b = 0xb3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef
 //! * Curve equation: y^2 = x^3 + ax + b
 
 #[cfg(feature = "r1cs")]
