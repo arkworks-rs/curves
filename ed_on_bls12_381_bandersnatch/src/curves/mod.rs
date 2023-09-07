66db16cf222036ce0b229181cffc210c5851f567
mmagician <marcin.gorny.94@protonmail.com>
2023-09-07 21:30:24 +0200

    temp remove Bandersnatch changes from PR

--- ed_on_bls12_381_bandersnatch/src/curves/mod.rs
+++ ed_on_bls12_381_bandersnatch/src/curves/mod.rs
@@ -1,10 +1,9 @@
 use ark_ec::{
     models::CurveConfig,
-    scalar_mul::glv::GLVConfig,
     short_weierstrass::{self, SWCurveConfig},
     twisted_edwards::{Affine, MontCurveConfig, Projective, TECurveConfig},
 };
-use ark_ff::{AdditiveGroup, BigInt, FpConfig, MontFp, PrimeField};
+use ark_ff::{AdditiveGroup, MontFp};
 
 use crate::{Fq, Fr};
 
@@ -120,112 +119,21 @@ const TE_GENERATOR_Y: Fq =
 
 /// x coordinate for SW curve generator
 const SW_GENERATOR_X: Fq =
-    MontFp!("4732093294267640299242820317528400560681136891967543338160850811774078125840");
+    MontFp!("30900340493481298850216505686589334086208278925799850409469406976849338430199");
 
 /// y coordinate for SW curve generator
 const SW_GENERATOR_Y: Fq =
-    MontFp!("31127102290931869693084292284935581507759552409643462510093198106308390504714");
+    MontFp!("12663882780877899054958035777720958383845500985908634476792678820121468453298");
 
-// We define the WS equation given in eprint/2021/1152
 impl SWCurveConfig for BandersnatchConfig {
-    /// COEFF_A = 52435875175126190479447740508185965837690552500527637822603658699934817984513
+    /// COEFF_A = 10773120815616481058602537765553212789256758185246796157495669123169359657269
     const COEFF_A: Self::BaseField =
-        MontFp!("52435875175126190479447740508185965837690552500527637822603658699934817984513");
+        MontFp!("10773120815616481058602537765553212789256758185246796157495669123169359657269");
 
-    /// COEFF_B = 52435875175126190479447740508185965837690552500527637822603658621262613184513
+    /// COEFF_B = 29569587568322301171008055308580903175558631321415017492731745847794083609535
     const COEFF_B: Self::BaseField =
-        MontFp!("52435875175126190479447740508185965837690552500527637822603658621262613184513");
+        MontFp!("29569587568322301171008055308580903175558631321415017492731745847794083609535");
 
     /// generators
     const GENERATOR: SWAffine = SWAffine::new_unchecked(SW_GENERATOR_X, SW_GENERATOR_Y);
-
-    #[inline]
-    fn mul_projective(p: &SWProjective, scalar: &[u64]) -> SWProjective {
-        let s = Self::ScalarField::from_sign_and_limbs(true, scalar);
-        GLVConfig::glv_mul_projective(*p, s)
-    }
-}
-
-impl GLVConfig for BandersnatchConfig {
-    const ENDO_COEFFS: &'static [Self::BaseField] = &[
-        MontFp!("44800"),
-        MontFp!("2257920000"),
-        MontFp!("26217937587563095239723870254092982918845276250263818911301829349969290592256"), // u² in eprint/2021/1152 page 5
-        MontFp!("8089994378958280414021351509578931277711957629993758335181991576135134307306"), // u³ in eprint/2021/1152 page 5
-        MontFp!("52435875175126190479447740508185965837690552500527637822603658699938330304513"), // t0 in eprint/2021/1152 page 5
-    ];
-
-    const LAMBDA: Self::ScalarField =
-        MontFp!("8913659658109529928382530854484400854125314752504019737736543920008458395397");
-
-    const SCALAR_DECOMP_COEFFS: [(bool, <Self::ScalarField as PrimeField>::BigInt); 4] = [
-        (true, BigInt!("113482231691339203864511368254957623327")),
-        (true, BigInt!("10741319382058138887739339959866629956")),
-        (false, BigInt!("21482638764116277775478679919733259912")),
-        (true, BigInt!("113482231691339203864511368254957623327")),
-    ];
-
-    fn endomorphism(base: &SWProjective) -> SWProjective {
-        /*
-        From the eprint, (x,y) \mapsto (u² (x² + 44800x + 2257920000)/(x+44800), u³ y (x² + 2*44800 x + t0) / (x+44800)²)
-        In Jacobian projective coordinates:
-        (x,y,z) \mapsto (u² (x²/z⁴ + 44800x/z² + 2257920000)/(x/z²+44800), u³ y/z³ (x²/z⁴ + 2*44800 x/z² + t0) / (x/²+44800)²)
-                      = (u² (x² + 44800x*z² + 2257920000*z⁴)/(x*z²+44800*z⁴), u³ y/z³ (x² + 2*44800 x*z² + t0*z⁴) / (x+44800*z²)²)
-                      = (u² (x² + 44800x*z² + 2257920000*z⁴)*(x+44800*z²), u³ y (x² + 2*44800 x*z² + t0*z⁴) *(x+44800*z²), z*(x+44800*z²))
-                      = (u² pol21 * pol1, u³ y pol22 * pol1, z * pol1)
-        */
-        let x = base.x;
-        let y = base.y;
-        let z = base.z;
-        let x2 = x * x;
-        let z2 = z * z;
-        let z3 = z2 * z;
-        let z4 = z3 * z;
-
-        let u2 = Self::ENDO_COEFFS[2];
-        let u3 = Self::ENDO_COEFFS[3];
-        let t0 = Self::ENDO_COEFFS[4];
-
-        // 44800xz²
-        let tmp0 = z2 * Self::ENDO_COEFFS[0];
-        let tmp1 = x * tmp0;
-        // (x² + 44800xz²)
-        let tmp3 = x2 + tmp1;
-        // (x² + 2 * 44800xz²)
-        let tmp4 = tmp3 + tmp1;
-
-        // num_x = u² (x² + 44800xz² + t0?????z⁴)
-        let num_x = u2 * (tmp3 + Self::ENDO_COEFFS[1] * z4);
-        // num_y = u³ y * (x² + 2 * 44800xz² + t0 z⁴)
-        let num_y = u3 * y * (tmp4 + t0 * z4);
-        let den = x + tmp0;
-
-        SWProjective::new_unchecked(num_x * den, num_y * den, den * z)
-    }
-
-    fn endomorphism_affine(p: &short_weierstrass::Affine<Self>) -> short_weierstrass::Affine<Self> {
-        // can be optimized
-
-        let var44800 = Self::ENDO_COEFFS[0];
-        let var2257920000 = Self::ENDO_COEFFS[1];
-        let u2 = Self::ENDO_COEFFS[2];
-        let u3 = Self::ENDO_COEFFS[3];
-        let t0 = Self::ENDO_COEFFS[4];
-        let x = p.x;
-        let y = p.y;
-
-        // 1/(x + 44800)
-        let tmp1 = FpConfig::inverse(&(x + var44800)).unwrap();
-        // 1/(x+44800)²
-        let tmp2 = tmp1 * tmp1;
-        // x * 44800
-        let tmp3 = x * var44800;
-        // x²
-        let x2 = x * x;
-        // numerator endo_x
-        let num_x = x2 + tmp3 + var2257920000;
-        // numerator endo_y
-        let num_y = x2 + tmp3 + tmp3 + t0;
-        SWAffine::new_unchecked(u2 * num_x * tmp1, u3 * y * num_y * tmp2)
-    }
 }
--- ed_on_bls12_381_bandersnatch/src/curves/tests.rs
+++ ed_on_bls12_381_bandersnatch/src/curves/tests.rs
@@ -2,4 +2,3 @@ use crate::*;
 use ark_algebra_test_templates::*;
 
 test_group!(te; EdwardsProjective; te);
-test_group!(glv; SWProjective; glv);
