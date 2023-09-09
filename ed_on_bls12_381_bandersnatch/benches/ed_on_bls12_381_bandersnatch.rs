b7d0f2243a755f8bd5f3c1a9c176548cf5e23e35
mmagician <marcin.gorny.94@protonmail.com>
2023-09-09 11:27:16 +0200

    remove benches and scripts that were not on master

--- ed_on_bls12_381_bandersnatch/benches/ed_on_bls12_381_bandersnatch.rs
+++
@@ -1,9 +0,0 @@
-use ark_algebra_bench_templates::*;
-use ark_ed_on_bls12_381_bandersnatch::{fq::Fq, fr::Fr, EdwardsProjective as G};
-
-bench!(
-    Name = "EdOnBls12Bandersnatch_381",
-    Group = G,
-    ScalarField = Fr,
-    PrimeBaseField = Fq,
-);
