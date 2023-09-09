05db67ccdcb170c752b49433c4ca491faa601624
mmagician <marcin.gorny.94@protonmail.com>
2023-09-09 11:32:07 +0200

    tests modules were duplicated - move glv tests to `tests` directory

--- bls12_377/src/curves/tests.rs
+++
@@ -1,9 +0,0 @@
-use crate::{Bls12_377, G1Projective, G2Projective};
-use ark_algebra_test_templates::*;
-
-test_group!(g1; G1Projective; sw);
-test_group!(g2; G2Projective; sw);
-test_group!(pairing_output; ark_ec::pairing::PairingOutput<Bls12_377>; msm);
-test_pairing!(pairing; crate::Bls12_377);
-test_group!(g1_glv; G1Projective; glv);
-test_group!(g2_glv; G2Projective; glv);
