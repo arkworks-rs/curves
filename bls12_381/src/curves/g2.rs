a135c29f2801a1d2d480e1bf161a4210e41e428b
Vehorny <153144728+vehorny@users.noreply.github.com>
2023-12-14 00:22:51 +0000

    typo fix

--- bls12_381/src/curves/g2.rs
+++ bls12_381/src/curves/g2.rs
@@ -295,7 +295,7 @@ fn double_p_power_endomorphism(p: &Projective<Config>) -> Projective<Config> {
     res
 }
 
-// Parametres from the [IETF draft v16, section E.3](https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-16.html#name-3-isogeny-map-for-bls12-381).
+// Parameters from the [IETF draft v16, section E.3](https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-16.html#name-3-isogeny-map-for-bls12-381).
 impl WBConfig for Config {
     type IsogenousCurve = g2_swu_iso::SwuIsoConfig;
 
