37e2ce82a84a6a4e655784a64733b7068c6d6d3c
CPerezz <c.perezbaro@gmail.com>
2023-12-18 16:01:13 +0100

    fix: typo

--- bn254/src/constraints/curves.rs
+++ bn254/src/constraints/curves.rs
@@ -2,7 +2,7 @@ use ark_r1cs_std::groups::curves::short_weierstrass::ProjectiveVar;
 
 use crate::{constraints::FBaseVar, g1::Config};
 
-/// A group element in the Grumpkin prime-order group.
+/// A group element in the Bn254 prime-order group.
 pub type GVar = ProjectiveVar<Config, FBaseVar>;
 
 #[test]
